4d7e1eb6b2d0b41a69024470340f8573d45210ac
amauribsjr <amauribsjr97@gmail.com>
2025-11-26 10:44:39 -0300

    change hardware.nix to label mode

--- hosts/nixos/hardware.nix
+++ hosts/nixos/hardware.nix
@@ -10,36 +10,19 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  #LABEL VERSION (FUTURE UPDATE)
-  # fileSystems."/" = {
-  #   device = "/dev/disk/by-label/NIXOS_ROOT";
-  #   fsType = "ext4";
-  # };
-  #
-  # fileSystems."/boot" = {
-  #   device = "/dev/disk/by-label/NIXOS_BOOT";
-  #   fsType = "vfat";
-  # };
-  #
-  # swapDevices = [{
-  #   device = "/dev/disk/by-label/NIXOS_SWAP";
-  # }];
-
-  #CURRENT SYSTEM
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/fa57f289-4eeb-495c-9238-63b57adf5269";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/486B-E851";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/dbe0302e-1453-4afb-80fa-c77af5b60443"; }
-    ];
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/NIXOS_ROOT";
+    fsType = "ext4";
+  };
+ 
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/NIXOS_BOOT";
+    fsType = "vfat";
+  };
+ 
+  swapDevices = [{
+    device = "/dev/disk/by-label/NIXOS_SWAP";
+  }];
 
   networking.useDHCP = lib.mkDefault true;
 
