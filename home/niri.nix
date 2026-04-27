d262443d840cad6cdb60d18011fdfd7ad7559ea8
Amauri <amauribsjunior@proton.me>
2026-04-27 14:13:01 -0300

    niri borders enchance (+)

--- home/niri.nix
+++ home/niri.nix
@@ -79,13 +79,15 @@
         open-floating = true;
       }
 
-      geometry-corner-radius = {
-        top-left = 2.0;
-        top-right = 2.0;
-        bottom-left = 2.0;
-        bottom-right = 2.0;
-      };
-      clip-to-geometry = true;
+      {
+        geometry-corner-radius = {
+          top-left = 2.0;
+          top-right = 2.0;
+          bottom-left = 2.0;
+          bottom-right = 2.0;
+        };
+        clip-to-geometry = true;
+      }
 
     ];
 
