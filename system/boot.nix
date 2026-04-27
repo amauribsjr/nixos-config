1efff651e6ccf307578200269eaaed8db273ee86
Amauri <amauribsjunior@proton.me>
2026-04-27 13:42:53 -0300

    figuring out why laptop is not shuting down properly2

--- system/boot.nix
+++ system/boot.nix
@@ -31,8 +31,9 @@
     };
   };
 
-  systemd.extraConfig = ''
-    DefaultTimeoutStopSec=10s
-    DefaultTimeoutAbortSec=10s
-  '';
+  systemd.settings.Manager = {
+      DefaultTimeoutStopSec = "10s";
+      DefaultTimeoutAbortSec = "10s";
+    };
+  }
 }
