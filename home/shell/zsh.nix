b6d7a5083c1fac250cf4f4b774ae1ac021e84ad9
Amauri <amauribsjunior@proton.me>
2026-04-27 13:59:13 -0300

    changed aliases for builds and tests

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -12,6 +12,8 @@
       rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos";
       update = "cd ~/nixos-config && nix flake update && sudo nixos-rebuild switch --flake .#nixos";
       clean = "sudo nix-collect-garbage -d";
+      cleanall = "sudo nix-collect-garbage --delete-older-than 1d";
+      testbuild = "cd ~/nixos-config && git add . && sudo nixos-rebuild test --flake ~/nixos-config#nixos";
     };
 
     initContent = ''
@@ -27,18 +29,21 @@
       # NixOS helper function
       function nixos() {
         if [ "$1" = "rebuild" ]; then
-          echo "🚀 Rebuilding NixOS..."
+          echo "🔄 Rebuilding NixOS..."
           sudo nixos-rebuild switch --flake ~/nixos-config#nixos
         elif [ "$1" = "update" ]; then
           echo "🔄 Updating flakes..."
           cd ~/nixos-config && nix flake update
-          echo "🚀 Rebuilding..."
+          echo "🔄 Rebuilding..."
           sudo nixos-rebuild switch --flake ~/nixos-config#nixos
         elif [ "$1" = "clean" ]; then
-          echo "🧹 Cleaning system..."
+          echo "🧹 Cleaning system 7 days old gens..."
           sudo nix-collect-garbage -d
+        elif [ "$1" = "cleanall" ]; then
+          echo "🧹 Cleaning 1 day old gens..."
+          sudo nix-collect-garbage --delete-older-than 1d
         else
-          echo "⚠️  Unknown command. Try: 'nixos rebuild', 'nixos update', or 'nixos clean'"
+          echo "⚠️ Unknown command."
         fi
       }
     '';
