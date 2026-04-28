52a9e8a18e21bfbed63a09181e4fe1e1bcd9e6ce
Amauri <amauribsjunior@proton.me>
2026-04-28 18:06:31 -0300

    debug - shutdown/poweroff

--- system/desktop/desktop.nix
+++ system/desktop/desktop.nix
@@ -13,6 +13,8 @@
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
     ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
+
+    ACTION=="add", SUBSYSTEM=="sound", ATTRS{idVendor}=="0a67", ATTRS{idProduct}=="d061", ENV{SYSTEMD_WANTS}=""
   '';
 
   xdg.portal = {
