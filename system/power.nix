1838e01b7b2458711983d4da56ed6d14cf10d652
Amauri <amauribsjunior@proton.me>
2026-04-28 01:04:19 -0300

    testing diff power energy fine tunes/2

--- system/power.nix
+++ system/power.nix
@@ -23,13 +23,12 @@
       CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
       CPU_BOOST_ON_BAT = 1;
-      CPU_MAX_PERF_ON_BAT = 85;
       CPU_HWP_DYN_BOOST_ON_AC = 1;
       CPU_HWP_DYN_BOOST_ON_BAT = 0;
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 60;
+      CPU_MAX_PERF_ON_BAT = 85;
 
       PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "low-power";
