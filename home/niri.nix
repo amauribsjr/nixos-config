00ec5df7ba22bedb4b71a8e90a214f058ef110f1
Amauri <amauribsjunior@proton.me>
2026-04-27 23:28:59 -0300

    changed corner radius for windows

--- home/niri.nix
+++ home/niri.nix
@@ -35,7 +35,7 @@ in
     };
 
     layout = {
-      gaps = 6;
+      gaps = 4;
       always-center-single-column = true;
       preset-column-widths = [
         { proportion = 1.0 / 3.0; }
@@ -84,10 +84,10 @@ in
 
       {
         geometry-corner-radius = {
-          top-left = 4.0;
-          top-right = 4.0;
-          bottom-left = 4.0;
-          bottom-right = 4.0;
+          top-left = 0.0;
+          top-right = 0.0;
+          bottom-left = 0.0;
+          bottom-right = 0.0;
         };
         clip-to-geometry = true;
       }
