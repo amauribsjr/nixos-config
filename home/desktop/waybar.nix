e9bc1ffa26e727b93be02eab2480dfe2b8585efd
Amauri <amauribsjunior@proton.me>
2026-04-28 16:04:12 -0300

    power off fine tune fixes

--- home/desktop/waybar.nix
+++ home/desktop/waybar.nix
@@ -3,7 +3,7 @@
 let
 power-menu = pkgs.writeShellScript "power-menu" ''
   chosen=$(printf "Power off\nReboot\nSuspend\nLogout" | wofi --dmenu \
-    --width=180 --height=180 --location=center \
+    --width=180 --height=220 --location=center \
     --style=/home/koppi/.config/wofi/center.css)
   if echo "$chosen" | grep -q "Power off"; then
     systemctl poweroff
