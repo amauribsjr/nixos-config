93584dedd4393cdc69869a8f483153d09382ebe6
amauribsjr <amauribsjunior@proton.me>
2026-05-03 21:02:54 -0300

    update: tlp fine tuning on ac

--- system/hardware/power.nix
+++ system/hardware/power.nix
@@ -17,44 +17,43 @@
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC    = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_AC  = "performance";
-      CPU_MIN_PERF_ON_AC            = 11;
+      CPU_MIN_PERF_ON_AC            = 0;
       CPU_MAX_PERF_ON_AC            = 100;
       CPU_BOOST_ON_AC               = 1;
       CPU_HWP_DYN_BOOST_ON_AC       = 1;
 
-      # CPU - BAT balanced real
+      # CPU - BAT balanced
       CPU_SCALING_GOVERNOR_ON_BAT    = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT  = "balance_power";
-      CPU_MIN_PERF_ON_BAT            = 11;
-      CPU_MAX_PERF_ON_BAT            = 80;
+      CPU_ENERGY_PERF_POLICY_ON_BAT  = "balance_performance";
+      CPU_MIN_PERF_ON_BAT            = 0;
+      CPU_MAX_PERF_ON_BAT            = 90;
       CPU_BOOST_ON_BAT               = 1;
       CPU_HWP_DYN_BOOST_ON_BAT       = 1;
 
-      # Perfil térmico/firmware Dell
+      # Thermal profile
       PLATFORM_PROFILE_ON_AC  = "balanced";
       PLATFORM_PROFILE_ON_BAT = "balanced";
 
       # PCIe / runtime power
-      RUNTIME_PM_ON_AC = "auto";
+      RUNTIME_PM_ON_AC = "on";
       RUNTIME_PM_ON_BAT = "auto";
       RUNTIME_PM_DRIVER_DENYLIST = "r8169";
 
       PCIE_ASPM_ON_AC  = "default";
       PCIE_ASPM_ON_BAT = "powersave";
 
-      # Wi-Fi
       WIFI_PWR_ON_AC  = "off";
       WIFI_PWR_ON_BAT = "on";
 
       WOL_DISABLE = "Y";
 
-      # Áudio
+      # Audio
       SOUND_POWER_SAVE_ON_AC      = 0;
       SOUND_POWER_SAVE_ON_BAT     = 1;
       SOUND_POWER_SAVE_CONTROLLER = "N";
 
       # USB
-      USB_AUTOSUSPEND = 1;
+      USB_AUTOSUSPEND = 0;
     };
   };
 
