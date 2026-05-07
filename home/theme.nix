d98ad35d49e3f8ee73bd5e7a5421baabae07a75a
amauribsjr <amauribsjunior@proton.me>
2026-05-07 10:28:41 -0300

    refactor(helix.nix hardcoded theme): koppi theme was still hardcoded within helix.nix; there's helix-theme.nix for it

--- home/theme.nix
+++ home/theme.nix
@@ -4,24 +4,24 @@
   home.pointerCursor = {
     name = "Bibata-Modern-Classic";
     package = pkgs.bibata-cursors;
-    size = 24;
+    size = 20;
     gtk.enable = true;
   };
 
   gtk = {
     enable = true;
-  
+
     theme = {
       name = "Gruvbox-Dark";
       package = pkgs.gruvbox-gtk-theme;
     };
-  
+
     gtk4.theme = {
       name = "Gruvbox-Dark";
       package = pkgs.gruvbox-gtk-theme;
     };
   };
-
+  
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
