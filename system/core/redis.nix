1530ab8e047a19fd47b7bf53733daab71815cb5a
amauribsjr <amauribsjunior@proton.me>
2026-05-07 20:29:14 -0300

    refactor(services): simplified services.redis into redis.nix

--- system/core/redis.nix
+++ system/core/redis.nix
@@ -3,28 +3,10 @@
 {
   services.redis = {
     vmOverCommit = true;
-
     servers.dev = {
       enable = true;
       bind = "127.0.0.1";
       port = 6379;
-      openFirewall = false;
-      databases = 16;
-
-      save = [
-        [ 900 1 ]
-        [ 300 10 ]
-        [ 60 10000 ]
-      ];
-
-      appendOnly = false;
-      appendFsync = "everysec";
-
-      settings = {
-        protected-mode = "yes";
-        maxmemory = "256mb";
-        maxmemory-policy = "allkeys-lru";
-      };
     };
   };
 }
