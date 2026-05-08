3804be9991bac997a2efd372f68ff0faae2abfd5
amauribsjr <amauribsjunior@proton.me>
2026-05-08 19:55:40 -0300

    no (for redis... maybe tomorrow)

--- system/core/networking.nix
+++ system/core/networking.nix
@@ -4,10 +4,12 @@
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
-    firewall.enable = true;
-    extraCommands = ''
-      iptables -A INPUT -s 172.16.0.0/12 -p tcp -m multiport --dports 3306,6379,8080 -j ACCEPT
-    '';
+    firewall = {
+      enable = true;
+      extraCommands = ''
+        iptables -A INPUT -s 172.16.0.0/12 -p tcp -m multiport --dports 3306,6379,8080 -j ACCEPT
+      '';
+    };
   };
 
   systemd.services.NetworkManager-wait-online.enable = false;
