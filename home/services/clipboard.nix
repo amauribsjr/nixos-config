6b7ca68797d35b3656155bb1fa1f6722c9d79ac1
amauribsjr <amauribsjunior@proton.me>
2026-05-18 19:39:23 -0300

    feat(clipboard & text editor): add a clipboard with cliphist + wl-clipboard + wofi; add gnome-text-editor

--- home/services/clipboard.nix
+++ home/services/clipboard.nix
@@ -2,10 +2,17 @@
 
 let
   clipboardMenu = pkgs.writeShellScriptBin "clipmenu" ''
-    selected="$(${pkgs.cliphist}/bin/cliphist list | ${pkgs.wofi}/bin/wofi --dmenu --prompt "clipboard > ")"
+    selected="$(${pkgs.cliphist}/bin/cliphist list |
+      ${pkgs.wofi}/bin/wofi \
+        --dmenu \
+        --conf "$HOME/.config/wofi/config" \
+        --style "$HOME/.config/wofi/center.css" \
+        --prompt "clipboard > ")"
 
     if [ -n "$selected" ]; then
-      printf '%s' "$selected" | ${pkgs.cliphist}/bin/cliphist decode | ${pkgs.wl-clipboard}/bin/wl-copy
+      printf '%s' "$selected" |
+        ${pkgs.cliphist}/bin/cliphist decode |
+        ${pkgs.wl-clipboard}/bin/wl-copy
     fi
   '';
 
@@ -13,6 +20,7 @@ let
     ${pkgs.cliphist}/bin/cliphist wipe
   '';
 in
+
 {
   home.packages = [
     pkgs.wl-clipboard
@@ -23,7 +31,6 @@ in
   services.cliphist = {
     enable = true;
     systemdTargets = [ "graphical-session.target" ];
-
     allowImages = true;
 
     extraOptions = [
